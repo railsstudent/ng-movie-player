f774f4487e56f06a71429c99d35163e26475b97f
Connie Leung <cleung334@gmail.com>
2020-06-21 20:38:22 +0800

    feat: store movies in local storage
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- src/app/shared/movie-player.service.ts
+++ src/app/shared/movie-player.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 import { Movie } from './movie';
 
 const API_URL = 'https://scotch-mvplayer-api.herokuapp.com/api/v1';
@@ -14,9 +15,15 @@ export class MoviePlayerService {
 
   getMovies() {
     const sortName = (a: Movie, b: Movie) => a.name.toLowerCase().localeCompare(b.name.toLowerCase());
+    if (localStorage.getItem('ng-movies')) {
+      const movies = JSON.parse(localStorage.getItem('ng-movies')) as Movie[];
+      return of(movies)
+    }
+
     return this.http.get<Movie[]>(API_URL)
       .pipe(
-        map(movies => movies.sort(sortName))
+        map(movies => movies.sort(sortName)),
+        tap(movies => localStorage.setItem('ng-movies', JSON.stringify(movies)))
       );
   }
 }
